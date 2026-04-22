f8c1b83b9db13f8c7c92f1f1f06ac03ff33eec98
belmqadem <adilfront3@gmail.com>
2026-04-22 16:15:40 +0100

    feat: implement rate limiting for authentication routes and add express-rate-limit dependency

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,12 +2,23 @@ const router = require("express").Router();
 const auth = require("../controllers/auth.controller");
 const csrf = require("../core/csrf");
 const withErrorHandling = require("../utils/withErrorHandling");
+const authLimiter = require("../middlewares/rateLimiter.middleware");
 
 router.get("/register", withErrorHandling(auth.getRegister));
-router.post("/register", csrf.verify, withErrorHandling(auth.postRegister));
+router.post(
+  "/register",
+  authLimiter,
+  csrf.verify,
+  withErrorHandling(auth.postRegister),
+);
 
 router.get("/login", withErrorHandling(auth.getLogin));
-router.post("/login", csrf.verify, withErrorHandling(auth.postLogin));
+router.post(
+  "/login",
+  authLimiter,
+  csrf.verify,
+  withErrorHandling(auth.postLogin),
+);
 
 router.post("/logout", csrf.verify, withErrorHandling(auth.logout));
 router.get("/verify", withErrorHandling(auth.getVerify));
@@ -15,11 +26,17 @@ router.get("/verify", withErrorHandling(auth.getVerify));
 router.get("/forgot-password", withErrorHandling(auth.getForgot));
 router.post(
   "/forgot-password",
+  authLimiter,
   csrf.verify,
   withErrorHandling(auth.postForgot),
 );
 
 router.get("/reset-password", withErrorHandling(auth.getReset));
-router.post("/reset-password", csrf.verify, withErrorHandling(auth.postReset));
+router.post(
+  "/reset-password",
+  authLimiter,
+  csrf.verify,
+  withErrorHandling(auth.postReset),
+);
 
 module.exports = router;
